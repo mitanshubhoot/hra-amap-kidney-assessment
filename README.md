b712493a12b5ad8588ddf3a37e384415135e4c88
Bhargav Snehal Desai <desabh@iu.edu>
2023-04-21 21:19:04 +0530

    updated readme.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # hra-amap
+
 Code for AMap project.
+
+Setup instructions:
+
+1. Clone the repository with ```git clone https://github.com/cns-iu/hra-amap.git```
+
+The repository already includes pre-built wheels and made files. In case of any build errors, please refer to the following https://github.com/ohirose/bcpd
+
+2. To run a quick registration using the provided pipeline, please see ```notebooks/pipeline_usage``` 
--- code/utils.py
+++ code/utils.py
@@ -4,11 +4,26 @@ import open3d as o3d
 
 from typing import Union
 
+
+def mesh_to_pointcloud(mesh: trimesh.base.Trimesh) -> o3d.geometry.PointCloud:
+    """Converts a trimesh mesh object to an open3d compatible point cloud"""
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(np.array(mesh.vertices))
+    pcd.estimate_normals()
+    return pcd
+
 def numpy_to_pointcloud(numpy_array: np.ndarray) -> o3d.geometry.PointCloud:
     """Converts a numpy array to an open3d compatible point cloud"""
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(numpy_array)
+    pcd.estimate_normals()
     return pcd
 
-def load(path: str) -> Union[o3d.geometry.PointCloud, np.ndarray]:
+def pointcloud_to_mesh(pointcloud: o3d.geometry.PointCloud, faces: np.ndarray) -> o3d.geometry.PointCloud:
+    """Converts a open3d point cloud object to trimesh mesh object"""
+    return trimesh.Trimesh(vertices=pointcloud.points, faces=faces)
+
+def load(file_path: str) -> Union[o3d.geometry.PointCloud, trimesh.base.Trimesh]:
+    """Loads a 3D asset from a local path"""
     raise NotImplementedError
+
